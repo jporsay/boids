110efacb6af5a3ca9fd6773cff1f2c63331bc1db
Juan Pablo Orsay <jporsay@gmail.com>
2018-11-24 20:24:26 -0300

    Add AvoidVoid

---
+++ src/main/kotlin/ar/edu/itba/ss/rules/AvoidBoid.kt
@@ -0,0 +1,27 @@
+package ar.edu.itba.ss.rules
+
+import ar.edu.itba.ss.extensions.minus
+import ar.edu.itba.ss.model.Entity
+import ar.edu.itba.ss.model.Type
+import ar.edu.itba.ss.model.Universe
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D
+
+class AvoidBoid(private val distance: Double): Rule {
+    override fun apply(entity: Entity, universe: Universe): Vector3D {
+        if (entity.type != Type.Boid) return Vector3D.ZERO
+
+        val neighbours = universe.getNear(entity)
+        if (neighbours.isEmpty()) return Vector3D.ZERO
+
+        var out = Vector3D.ZERO
+        val awkwardNeighbours = neighbours.filter { it.position.distance(entity.position) < distance }
+
+        if (awkwardNeighbours.isEmpty()) return Vector3D.ZERO
+
+        awkwardNeighbours.forEach {
+            out -= (it.position - entity.position)
+        }
+        return out
+    }
+
+}
