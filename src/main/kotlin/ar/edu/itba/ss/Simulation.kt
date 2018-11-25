1c243b64cee356f1e7b3bcad851b696d2f0528b8
Juan Pablo Orsay <jporsay@gmail.com>
2018-11-25 06:00:08 -0300

    Clamping velocity

--- src/main/kotlin/ar/edu/itba/ss/Simulation.kt
+++ src/main/kotlin/ar/edu/itba/ss/Simulation.kt
@@ -6,7 +6,13 @@ import ar.edu.itba.ss.model.Universe
 import ar.edu.itba.ss.rules.Rule
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D
 
-class Simulation(var universe: Universe, private val rules: List<Rule>, private val dT: Double) {
+class Simulation(
+    var universe: Universe,
+    private val rules: List<Rule>,
+    private val dT: Double,
+    val limitSpeed: Boolean,
+    val maxSpeed: Double
+) {
 
     // http://www.kfish.org/boids/pseudocode.html
     fun step(): Universe {
@@ -16,6 +22,7 @@ class Simulation(var universe: Universe, private val rules: List<Rule>, private 
             rules.forEach {
                 velocity = velocity.add(it.apply(entity, universe))
             }
+            velocity = clampVelocity(velocity)
             val position = entity.position + velocity * dT
             val builder = Entity.Builder(entity)
             builder.velocity = velocity
@@ -24,6 +31,12 @@ class Simulation(var universe: Universe, private val rules: List<Rule>, private 
         }
         universe = universeBuilder.build()
         return universe
+    }
+
+    private fun clampVelocity(velocity: Vector3D): Vector3D {
+        if (!limitSpeed) return velocity
+        if (velocity.norm <= maxSpeed) return velocity
+        return velocity.normalize() * maxSpeed
     }
 
     private fun clampPosition(position: Vector3D): Vector3D = Vector3D(
--- src/main/kotlin/ar/edu/itba/ss/command/Simulate.kt
+++ src/main/kotlin/ar/edu/itba/ss/command/Simulate.kt
@@ -3,11 +3,13 @@ package ar.edu.itba.ss.command
 import ar.edu.itba.ss.Simulation
 import ar.edu.itba.ss.io.UniverseExporter
 import ar.edu.itba.ss.io.UniverseImporter
+import ar.edu.itba.ss.rules.AvoidBoid
 import ar.edu.itba.ss.rules.CenterOfMass
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.double
 import com.github.ajalt.clikt.parameters.types.int
 
 class Simulate : CliktCommand(help = "Simulate a given universe") {
@@ -26,12 +28,18 @@ class Simulate : CliktCommand(help = "Simulate a given universe") {
     private val outputPathDefault = "universe.sim.xyz"
     private val outputPath: String by option(help = "Simulation output file. Default '$outputPathDefault'").default(outputPathDefault)
 
+    private val limitSpeed: Boolean by option(help = "Enable boid speed limit").flag()
+
+    private val maxSpeedDefault = 20.0
+    private val maxSpeed: Double by option(help = "Maximum void speed. Default '$maxSpeedDefault'").double().default(maxSpeedDefault)
+
     override fun run() {
         val universe = UniverseImporter(inputPath).next()
         val dT = 1.0 / fps
         val simulation = Simulation(universe, listOf(
-            CenterOfMass(100.0)
-        ), dT)
+            CenterOfMass(100.0),
+            AvoidBoid(1.0)
+        ), dT, limitSpeed, maxSpeed)
         UniverseExporter(outputPath).use { exporter ->
             exporter.write(universe)
             for (frame in 1..(seconds * fps)) {
