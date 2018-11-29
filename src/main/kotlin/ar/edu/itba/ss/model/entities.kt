01dd45b4f87b45d1cdd5b35b053bbda3d3259774
Qcho <qcho86@gmail.com>
2018-11-29 00:16:05 -0300

    faster angle function

--- src/main/kotlin/ar/edu/itba/ss/model/entities.kt
+++ src/main/kotlin/ar/edu/itba/ss/model/entities.kt
@@ -3,6 +3,8 @@ package ar.edu.itba.ss.model
 import ar.edu.itba.ss.extensions.toXYZ
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D
 import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.sqrt
 
 open class Entity(val id: Int, val type: Type, val radius: Double, val position: Vector3D, val velocity: Vector3D) {
 
@@ -17,10 +19,32 @@ open class Entity(val id: Int, val type: Type, val radius: Double, val position:
             Added some extra checks just in case
          */
         val maxVisionAngle = PI * 0.75
-        var angle = Vector3D.angle(velocity, other.velocity)
+        var angle = angle(velocity, other.velocity)
         if (angle < 0) angle += 2 * PI
         if (angle > 2 * PI) angle -= 2 * PI
         return (angle < maxVisionAngle) || (angle > 1.25 * PI)
+    }
+
+    private fun angle(v1: Vector3D, v2: Vector3D) : Double {
+        val mag2 = v1.x * v1.x + v1.y * v1.y + v1.z * v1.z
+        val vmag2 = v2.x * v2.x + v2.y * v2.y + v2.z * v2.z
+        val dot = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+        return acos(dot / sqrt(mag2 * vmag2))
+    }
+
+    private fun acos(x: Double): Double {
+        val negate = if (x < 0) 1.0 else 0.0
+        val xabs = abs(x)
+        var ret = -0.0187293
+        ret *= xabs
+        ret += 0.0742610
+        ret *= xabs
+        ret -= 0.2121144
+        ret *= xabs
+        ret += 1.5707288
+        ret *= sqrt(1.0 - xabs)
+        ret -= 2 * negate * ret
+        return negate * 3.14159265358979 + ret
     }
 
     class Builder() {
