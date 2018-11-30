65a7123a35ebf9661e66fa28f06ee116a9db3033
Qcho <qcho86@gmail.com>
2018-11-30 17:24:21 -0300

    corutines map and foreach methods.

--- src/main/kotlin/ar/edu/itba/ss/extensions/extensions.kt
+++ src/main/kotlin/ar/edu/itba/ss/extensions/extensions.kt
@@ -1,5 +1,8 @@
 package ar.edu.itba.ss.extensions
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D
 
 fun Vector3D.toXYZ(): String {
@@ -13,3 +16,10 @@ operator fun Vector3D.minus(other: Vector3D): Vector3D = this.subtract(other)
 
 operator fun Vector3D.plus(other: Vector3D): Vector3D = this.add(other)
 
+fun <A, B>Iterable<A>.pmap(f: suspend (A) -> B): List<B> = runBlocking {
+    map { async { f(it) } }.map { it.await() }
+}
+
+fun <A, B>Iterable<A>.pforEach(f: suspend (A) -> B): Unit = runBlocking(Dispatchers.Default) {
+    map { async { f(it) } }.forEach { it.await() }
+}
