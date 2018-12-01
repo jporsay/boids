fc3b3e8923017d2f2a8824aa101c0d97d6fa986d
Juan Pablo Orsay <jporsay@gmail.com>
2018-12-01 19:25:35 -0300

    Rename parameters

--- src/main/kotlin/ar/edu/itba/ss/rules/TendencyTo.kt
+++ src/main/kotlin/ar/edu/itba/ss/rules/TendencyTo.kt
@@ -5,13 +5,13 @@ import ar.edu.itba.ss.model.Type
 import ar.edu.itba.ss.model.Universe
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D
 
-class TendencyTo(factor: Double, private val appliesTo: Type, private val type: Type): Rule(factor) {
+class TendencyTo(factor: Double, private val appliesTo: Type, private val interactsWith: Type): Rule(factor) {
 
     override fun appliesTo(type: Type): Boolean = type == appliesTo
 
     override fun doApply(entity: Entity, neighbours: List<Entity>, universe: Universe): Vector3D {
         return neighbours
-            .filter { it.type == type }
+            .filter { it.type == interactsWith }
             .fold(Vector3D.ZERO) {acc, typeEntity -> acc.add(
                 typeEntity.position
                     .subtract(entity.position)
